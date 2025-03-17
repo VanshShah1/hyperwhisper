c839fccb6dd7d591c27b89e9636ff4e730947a20
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2025-03-17 11:10:58 -0700

    decdfvdfv

--- main.py
+++ main.py
@@ -1,46 +1,46 @@
 import flet as ft
 
-async def main(page: ft.Page):
+def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.appbar = ft.AppBar(title=ft.Text("Audio Recorder"), center_title=True)
 
     path = "test-audio-file.wav"
 
-    async def handle_start_recording(e):
+    def handle_start_recording(e):
         print(f"StartRecording: {path}")
-        await audio_rec.start_recording_async(path)
+        audio_rec.start_recording(path)
 
-    async def handle_stop_recording(e):
-        output_path = await audio_rec.stop_recording_async()
+    def handle_stop_recording(e):
+        output_path = audio_rec.stop_recording()
         print(f"StopRecording: {output_path}")
         if page.web and output_path is not None:
-            await page.launch_url_async(output_path)
+            page.launch_url(output_path)
 
-    async def handle_list_devices(e):
-        devices = await audio_rec.get_input_devices_async()
+    def handle_list_devices(e):
+        devices = audio_rec.get_input_devices()
         print(devices)
 
-    async def handle_has_permission(e):
+    def handle_has_permission(e):
         try:
-            print(f"HasPermission: {await audio_rec.has_permission_async()}")
+            print(f"HasPermission: {audio_rec.has_permission()}")
         except Exception as e:
             print(e)
 
-    async def handle_pause(e):
-        print(f"isRecording: {await audio_rec.is_recording_async()}")
-        if await audio_rec.is_recording_async():
-            await audio_rec.pause_recording_async()
+    def handle_pause(e):
+        print(f"isRecording: {audio_rec.is_recording()}")
+        if audio_rec.is_recording():
+            audio_rec.pause_recording()
 
-    async def handle_resume(e):
-        print(f"isPaused: {await audio_rec.is_paused_async()}")
-        if await audio_rec.is_paused_async():
-            await audio_rec.resume_recording_async()
+    def handle_resume(e):
+        print(f"isPaused: {audio_rec.is_paused()}")
+        if audio_rec.is_paused():
+            audio_rec.resume_recording()
 
-    async def handle_audio_encoding_test(e):
+    def handle_audio_encoding_test(e):
         for i in list(ft.AudioEncoder):
-            print(f"{i}: {await audio_rec.is_supported_encoder_async(i)}")
+            print(f"{i}: {audio_rec.is_supported_encoder(i)}")
 
-    async def handle_state_change(e):
+    def handle_state_change(e):
         print(f"State Changed: {e.data}")
 
     audio_rec = ft.AudioRecorder(
@@ -60,5 +60,4 @@ async def main(page: ft.Page):
         ft.ElevatedButton("Has Permission", on_click=handle_has_permission),
     )
 
-
-ft.app(main)
+ft.app(target=main)
